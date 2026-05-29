b3ca62ba4c5826500f4d587b04af02978e6e7c19
yasin.jsdev <yasin.jsdev@gmail.com>
2026-05-29 14:37:31 +0530

    working on comments feature

---
+++ blog-app/app/api/blog/[id]/like/route.ts
@@ -0,0 +1,21 @@
+// import { ConnectDb } from "@/lib/monogoose";
+// import { NextRequest, NextResponse } from "next/server";
+
+// export async function POST(
+//   req: NextRequest,
+//   {
+//     params: {
+//       params: { id: string },
+//     },
+//   },
+// ) {
+//   try {
+//     await ConnectDb();
+//   } catch (error) {
+//     console.log(error);
+//     return NextResponse.json({
+//       success: false,
+//       message: "something went wrong",
+//     });
+//   }
+// }
---
+++ blog-app/models/likes.ts
@@ -0,0 +1,30 @@
+import { timeStamp } from "console";
+import mongoose, { Document, Schema } from "mongoose";
+
+interface LikeInterface extends Document {
+  user: mongoose.Types.ObjectId;
+  blog: mongoose.Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const LikeSchema = new Schema<LikeInterface>(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    blog: {
+      type: mongoose.Types.ObjectId,
+      ref: "Blog",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+LikeSchema.index({ user: 1, blog: 1 }, { unique: true });
+
+const Like =
+  mongoose.models.Like || mongoose.model<LikeInterface>("Like", LikeSchema);
