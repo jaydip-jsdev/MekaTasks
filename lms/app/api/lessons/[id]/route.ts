11d77500ca3cc36198d486a4a4179b4e276147f1
yasin.jsdev <yasin.jsdev@gmail.com>
2026-06-02 14:39:34 +0530

    backend is done for phase one related operations

--- lms/app/api/lessons/[id]/route.ts
+++ lms/app/api/lessons/[id]/route.ts
@@ -1,7 +1,12 @@
 import LessonsModel from "@/app/models/LessonsModel";
 import { ApiError, ApiSuccess } from "@/lib/api-response";
 import ConnectDB from "@/lib/db";
+
 import { NextRequest } from "next/server";
+import path from "path";
+import fs from "fs/promises";
+import CourseModel from "@/app/models/CourseModel";
+import { AdminAuth } from "@/lib/adminAuth";
 
 export async function GET(
   req: NextRequest,
@@ -37,38 +42,131 @@ export async function PATCH(
   try {
     await ConnectDB();
 
+    const auth = AdminAuth(req);
+    if (!auth.success) {
+      return ApiSuccess(auth.message || "authentication error");
+    }
+    
     const { id } = await context.params;
 
-    const formData = await req.formData();
-    const title = formData.get("title");
-    const description = formData.get("description");
-    const slug = formData.get("slug");
-    const courseId = formData.get("courseId");
-    const lesson = formData.get("lesson");
-
-    const updates: Record<string, any> = {};
-
-    if (title !== null) updates.title = title;
-    if (description !== null) updates.description = description;
-    if (slug !== null) updates.slug = slug;
-    if (courseId !== null) updates.courseId = courseId;
-    if (lesson !== null) updates.lesson = lesson;
-
-    if (slug) {
-      const exist = await LessonsModel.findOne({ slug });
-      if (exist) return ApiError("lesson already exist with slug " + slug, 400);
+    if (!id) {
+      return ApiError("Lesson ID is required", 400);
     }
 
     const dbLesson = await LessonsModel.findById(id);
-    if (!dbLesson) return ApiError("Lesson not found", 404);
+
+    if (!dbLesson) {
+      return ApiError("Lesson not found", 404);
+    }
+
+    const formData = await req.formData();
+
+    const title = formData.get("title") as string | null;
+    const description = formData.get("description") as string | null;
+    const slug = formData.get("slug") as string | null;
+    const courseId = formData.get("courseId") as string | null;
+    const lesson = formData.get("lesson") as File | null;
+
+    const updates: Record<string, any> = {};
+
+    if (title) updates.title = title;
+    if (description) updates.description = description;
+    if (courseId) updates.courseId = courseId;
+
+    if (slug) {
+      const exist = await LessonsModel.findOne({
+        slug,
+        _id: { $ne: id },
+      });
+
+      if (exist) {
+        return ApiError("Lesson already exists with slug " + slug, 400);
+      }
+
+      updates.slug = slug;
+    }
+
+    if (lesson && lesson.size > 0) {
+      if (!lesson.type.startsWith("video/")) {
+        return ApiError("Only video files are allowed", 400);
+      }
+
+      const bytes = await lesson.arrayBuffer();
+      const buffer = Buffer.from(bytes);
+
+      const uploadDir = path.join(process.cwd(), "public/uploads");
+      await fs.mkdir(uploadDir, { recursive: true });
+
+      const fileName = `${Date.now()}~${lesson.name}`;
+      const filePath = path.join(uploadDir, fileName);
+
+      await fs.writeFile(filePath, buffer);
+
+      if (dbLesson.video_url) {
+        try {
+          const oldFilePath = path.join(
+            process.cwd(),
+            "public",
+            dbLesson.video_url,
+          );
+
+          await fs.unlink(oldFilePath);
+        } catch (err) {
+          console.log("Old file not found");
+        }
+      }
+
+      updates.video_url = `/uploads/${fileName}`;
+    }
+
+    if (courseId && courseId.toString() !== dbLesson.courseId.toString()) {
+      await CourseModel.findByIdAndUpdate(dbLesson.courseId, {
+        $pull: {
+          lessons: dbLesson._id,
+        },
+      });
+
+      await CourseModel.findByIdAndUpdate(courseId, {
+        $push: {
+          lessons: dbLesson._id,
+        },
+      });
+    }
 
     const updatedLesson = await LessonsModel.findByIdAndUpdate(id, updates, {
       new: true,
     });
 
-    return ApiSuccess("Lesson Updated successfully", updatedLesson);
+    return ApiSuccess("Lesson updated successfully", updatedLesson, 200);
   } catch (error) {
     console.error(error);
-    return ApiError("Server error", 500);
+    return ApiError(
+      error instanceof Error ? error.message : "Server Error",
+      500,
+    );
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  context: {
+    params: { id: string };
+  },
+) {
+  try {
+    await ConnectDB();
+
+    const auth = AdminAuth(req);
+    if (!auth.success) {
+      return ApiError(auth.message || "Access Denied", auth.status);
+    }
+
+    const { id } = await context.params;
+
+    await LessonsModel.findByIdAndDelete(id);
+    return ApiSuccess("deleted");
+  } catch (error) {
+    console.error(error);
+    return ApiError("Server error");
   }
 }
