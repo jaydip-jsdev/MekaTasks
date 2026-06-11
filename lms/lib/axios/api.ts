1fcf9e72e25d73670b8444a0557ef2d34ae11609
yasin.jsdev <yasin.jsdev@gmail.com>
2026-06-11 16:35:23 +0530

    seprated endpoints

--- lms/lib/axios/api.ts
+++ lms/lib/axios/api.ts
@@ -1,79 +1,80 @@
 import { LoginPayload, RegisterPayload } from "@/Types/auth";
 import axiosInstance from "./axios";
 import { CreateCategoryPayload } from "@/Types/category";
+import endpoints from "./endpoints";
 
 // auth
+const auth = endpoints.auth;
 export const register = (payload: RegisterPayload) => {
-  return axiosInstance.post("register", payload);
+  return axiosInstance.post(auth.register, payload);
 };
 
 export const login = (payload: LoginPayload) => {
-  return axiosInstance.post("login", payload);
+  return axiosInstance.post(auth.login, payload);
 };
 
 export const logout = () => {
-  return axiosInstance.post("logout");
+  return axiosInstance.post(auth.logout);
 };
 
 // courses crud
+const course = endpoints.courses;
 export const GetCourses = (catId?: string, search?: string) => {
   const params = new URLSearchParams();
 
-  if (catId) {
-    params.append("categoryId", catId);
-  }
+  if (catId) params.append("categoryId", catId);
 
-  if (search) {
-    params.append("search", search);
-  }
+  if (search) params.append("search", search);
 
-  const endpoint = params.toString()
-    ? `courses?${params.toString()}`
-    : "courses";
+  let endpoint = course.list;
+
+  if (params.toString()) endpoint += `?${params.toString()}`;
 
   return axiosInstance.get(endpoint);
 };
 
 export const getCourseDetails = (slug: string) => {
-  return axiosInstance.get("courses/" + slug);
+  return axiosInstance.get(course.details(slug));
 };
 
 export const AddCourse = (payload: FormData) => {
-  return axiosInstance.post("courses/create", payload);
+  return axiosInstance.post(course.create, payload);
 };
 
 export const EditCourse = (slug: string, payload: FormData) => {
-  return axiosInstance.patch("courses/" + slug, payload);
+  return axiosInstance.patch(course.update(slug), payload);
 };
 
 export const DeleteCourse = (slug: string) => {
-  return axiosInstance.delete("courses/" + slug);
+  return axiosInstance.delete(course.delete(slug));
 };
 
 export const EnrollCourse = (courseId: string) => {
-  return axiosInstance.post("courses/enroll", { courseId });
+  return axiosInstance.post(course.enroll, { courseId });
 };
 
 export const enrolledCourses = () => {
-  return axiosInstance.get("my/enrolledcourses");
+  return axiosInstance.get(course.enrolled);
 };
 
 // categories crud
+const cats = endpoints.categories;
 export const addCategory = (payload: CreateCategoryPayload) => {
-  return axiosInstance.post("categories/create", payload);
+  return axiosInstance.post(cats.create, payload);
 };
 
 export const GetCategories = () => {
-  return axiosInstance.get("categories");
+  return axiosInstance.get(cats.list);
 };
 
 export const DeleteCategories = (id: string) => {
-  return axiosInstance.delete("categories/" + id);
+  return axiosInstance.delete(cats.delete(id));
 };
 
 // lessons crud
+const lessons = endpoints.lessons;
 export const UploadLesson = (payload: FormData) => {
-  return axiosInstance.post("lessons/create", payload, {
+  return axiosInstance.post(lessons.create, payload, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
@@ -81,22 +82,23 @@ export const UploadLesson = (payload: FormData) => {
 };
 
 export const GetLessonById = (id: string) => {
-  return axiosInstance.get("lessons/" + id);
+  return axiosInstance.get(lessons.details(id));
 };
 
 export const EditLesson = (id: string, payload: FormData) => {
-  return axiosInstance.patch("lessons/" + id, payload);
+  return axiosInstance.patch(lessons.update(id), payload);
 };
 
 export const DeleteLesson = (id: string) => {
-  return axiosInstance.delete("lessons/" + id);
+  return axiosInstance.delete(lessons.delete(id));
 };
 
 // History crud
+const history = endpoints.history;
 export const AddToHistory = (lessonId: string) => {
-  return axiosInstance.post("my/history/" + lessonId);
+  return axiosInstance.post(history.add(lessonId));
 };
 
 export const getHistory = () => {
-  return axiosInstance.get("my/history");
+  return axiosInstance.get(history.list);
 };
